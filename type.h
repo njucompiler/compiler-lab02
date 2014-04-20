9b6f7b50f1770d4950dd1587e12a67ec7d24eab5
Hiram 111220120 <303749053@qq.com>
2014-04-20 19:54:52 +0800

    SDT for exp and specifer

--- type.h
+++ type.h
@@ -4,8 +4,7 @@ struct Type_
 {
 	enum { INT, FLOAT, ARRAY, STRUCTURE, FUNC } kind;
 	union
-	{
-		// 基本类型
+		{// 基本类型
 		int INT;
 		float FLOAT;
 		// 数组类型信息包括元素类型与数组大小构成
@@ -19,25 +18,17 @@ struct Type_
 };
 struct FieldList_
 {
-	char* name;			// 域的名字
-	Type type;			// 域的类型
-	FieldList *child;		// 下一个域
-	FieldList *brother;		
-};
-struct FUNC{
+	char* name;	// 域的名字
+	Type type;	// 域的类型
+	FieldList *child;	// 下一个域
+	FieldList *brother;	
+	};
+	struct FUNC{
 	int declare;	//0声明，1实现
-<<<<<<< HEAD
-	enum{ VOID, INT, FLOAT, ARRAY, STRUCTURE }RETURN;	//返回值类型
-	FuncReturn *brother;
-};
-struct FuncReturn{
-	enum{INT, FLOAT, ARRAY, STRUCTURE}kind;
-=======
 	char RETURN[20];	//返回值类型
 	FuncVar *brother;
-}
+};
 struct FuncVar{
 	char kind[20];
->>>>>>> 3e39625ae97f9ae85e846ac5da2c8694810d87fb
 	RETURN *next;
 };
